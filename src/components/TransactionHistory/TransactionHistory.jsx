a1cafe09225eb58aef39958bae1c8d0d3c454faa
Osman <157528072+osmnr@users.noreply.github.com>
2025-03-13 23:42:40 +0300

    css updated for friendlist and transaction components
    css updated for friendlist and transaction components

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,18 +5,18 @@ const TransactionHistory = ({ transactions }) => {
     <table className={css.transactionTable}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.trxHeaders}>Type</th>
+          <th className={css.trxHeaders}>Amount</th>
+          <th className={css.trxHeaders}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {transactions.map(({ id, type, amount, currency }) => (
           <tr key={id} className={css.transactionRow}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+            <td className={css.transactionRow}>{type}</td>
+            <td className={css.transactionRow}>{amount}</td>
+            <td className={css.transactionRow}>{currency}</td>
           </tr>
         ))}
       </tbody>
